2cfee6fa71e3fea942e811bb2e77fedcb7f123ea
arpan sahu <arpanrocks95@gmail.com>
2020-07-02 16:00:10 +0530

    Update Largest Sum  Contiguous Subarray[kadanes algorithm].cpp

--- Largest Sum  Contiguous Subarray[kadanes algorithm].cpp
+++ Largest Sum  Contiguous Subarray[kadanes algorithm].cpp
@@ -1,33 +1,142 @@
 #include <iostream>
-#include<vector>
-#include <limits.h>
-
+#include <algorithm>
+#include <unordered_map>
+#include <cstring>
+#include <vector>
+#include <map>
+#include <climits>
 
 using namespace std;
 
-template<size_t  n>
-void LargestSumContiguousSubarray(int (&arr)[n])
-{
-    int sum = INT_MIN;
-    int tempsum  = 0;
-    int last_index = 0;
-    int len = 0;
-    for (int i = 0; i < n; ++i) {
-        tempsum += arr[i];
-        if(sum < tempsum) {sum = tempsum;len = i - last_index ;last_index = i;}
-        if(tempsum < 0 )tempsum = 0;//cout << sum <<" " << tempsum <<"\n";
-    }
-    cout << "Maximum sum is :" <<sum <<"\t";
-    for (int j = last_index - len ; j <= last_index; ++j) cout << arr[j] <<" ";
+int kadaneForPositiveNegative (int arr[],int n){
+    int max_so_far = 0;
+    int max_ending_here = 0;
 
+    for (int i = 0; i < n; ++i) {
+        max_ending_here = max_ending_here + arr[i];
+        max_ending_here = max(max_ending_here,0);
+        max_so_far = max(max_so_far,max_ending_here);
+    }
+    return max_so_far;
+}
+
+void printkadaneForPositiveNegative (int arr[],int n){
+    int max_so_far = 0;
+    int max_ending_here = 0;
+    int lastIndex = 0;
+    int startIndex = 0;
+    int s = 0;
+    for (int i = 0; i < n; ++i) {
+        max_ending_here = max_ending_here + arr[i];
+
+        if(max_ending_here < 0 ){
+            max_ending_here = 0;
+            s = i + 1;
+        }
+
+        int check  = max_so_far;
+        max_so_far = max(max_so_far,max_ending_here);
+        if(check != max_so_far) {
+            startIndex = s;
+            lastIndex = i ;
+        }
+    }
+    cout<<"Using Kadane Mix :- Max sum is : "<< max_so_far << "   ";
+    for (int j =  startIndex; j <= lastIndex ; ++j) cout<<arr[j]<<" ";cout<<endl;
+}
+
+int kadaneForAllNegative (int arr[],int n){
+    int max_so_far = arr[0];
+    int max_ending_here = arr[0];
+
+    for (int i = 1; i < n; ++i) {
+        max_ending_here = max_ending_here + arr[i];
+        max_ending_here = max(max_ending_here,arr[i]);
+        max_so_far = max(max_so_far,max_ending_here);
+    }
+    return max_so_far;
+}
+
+void printkadaneForAllNegative (int arr[],int n){
+    int max_so_far = arr[0];
+    int max_ending_here = arr[0];
+
+    int lastIndex = 0;
+    int startIndex = 0;
+    int s = 0;
+
+    for (int i = 1; i < n; ++i) {
+        max_ending_here = max_ending_here + arr[i];
+
+        if(max_ending_here < arr[i]){
+            s = i ;
+            max_ending_here = arr[i];
+        }
+        int check = max_so_far;
+        max_so_far = max(max_so_far,max_ending_here);
+        if(check != max_so_far) {
+            startIndex = s;
+            lastIndex = i ;
+        }
+    }
+
+    cout<<"Using Kadane NEgative :-  Max sum is : "<< max_so_far << "   ";
+    for (int j =  startIndex; j <= lastIndex ; ++j) cout<<arr[j]<<" ";cout<<endl;
 }
 
 
-// main function
+int kadaneUniversal(int arr[],int n){
+    int max_so_far = INT_MIN;
+    int max_ending_here = arr[0];
+
+    for (int i = 0; i < n; ++i) {
+        max_ending_here = max_ending_here + arr[i];
+        max_so_far = max(max_so_far,max_ending_here);
+        max_ending_here = max(max_ending_here,0);
+    }
+    return max_so_far;
+}
+
+void printkadaneUniversal (int arr[],int n){
+    int max_so_far = INT_MIN;
+    int max_ending_here = 0;
+    int lastIndex = 0;
+    int startIndex = 0;
+    int s = 0;
+    for (int i = 0; i < n; ++i) {
+        max_ending_here = max_ending_here + arr[i];
+
+        int check  = max_so_far;
+        max_so_far = max(max_so_far,max_ending_here);
+        if(check != max_so_far) {
+            startIndex = s;
+            lastIndex = i ;
+        }
+
+        if(max_ending_here < 0 ){
+            max_ending_here = 0;
+            s = i + 1;
+        }
+
+    }
+    cout<<"Using Kadane Universal :- Max sum is : "<< max_so_far << "   ";
+    for (int j =  startIndex; j <= lastIndex ; ++j) cout<<arr[j]<<" ";cout<<endl;
+}
+
 int main()
 {
-    int arr[] =  {-2, -3, 4, -1, -2, 1, 5, -3};
-    LargestSumContiguousSubarray(arr);
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+    int arr1[] = { -8, -3, -6, -2, -5, -4 };
+
+    cout << "Using Kadane Mix The sum of contiguous sub-array with the largest sum is " <<kadaneForPositiveNegative(arr, (*(&arr+1)-arr))<<endl;
+    printkadaneForPositiveNegative(arr,(*(&arr+1)-arr));
+    cout << "Using kadane Negative The sum of contiguous sub-array with the largest sum is " <<kadaneForAllNegative(arr1, (*(&arr1+1)-arr1))<<endl;
+    printkadaneForAllNegative(arr1,(*(&arr1+1)-arr1));
+
+    cout << "Using Kadane Universal The sum of contiguous sub-array with the largest sum is " <<kadaneUniversal(arr, (*(&arr+1)-arr))<<endl;
+    printkadaneUniversal(arr,(*(&arr+1)-arr));
+    cout << "Using Kadane Universal The sum of contiguous sub-array with the largest sum is " <<kadaneUniversal(arr1, (*(&arr1+1)-arr1))<<endl;
+    printkadaneUniversal(arr1,(*(&arr1+1)-arr1));
 
     return 0;
 }
